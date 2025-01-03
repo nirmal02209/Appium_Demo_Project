d60bccdae6c2d763b3eb89926f2f340f1af6c434
nirmal02209 <nirmal02209@gmail.com>
2025-01-03 18:13:44 +0530

    push the code

--- src/main/java/com/example/appium/LoginTest.java
+++ src/main/java/com/example/appium/LoginTest.java
@@ -7,19 +7,16 @@ public class LoginTest {
 
     @Test
     public void printSecrets() {
-        Properties properties = new Properties();
-
-        // Load secrets from environment variables
-        String username = System.getenv("USERNAME");
-        String password = System.getenv("PASSWORD");
+        // Retrieve credentials from environment variables
+        String username = System.getenv("APP_USERNAME");
+        String password = System.getenv("APP_PASSWORD");
 
         // Set properties
-        properties.setProperty("USERNAME", username);
-        properties.setProperty("PASSWORD", password);
+
 
         // Print the secrets
-        System.out.println("USERNAME: " + properties.getProperty("USERNAME"));
-        System.out.println("PASSWORD: " + properties.getProperty("PASSWORD"));
+        System.out.println("USERNAME: " + username);
+        System.out.println("PASSWORD: " + password);
     }
 
     }
