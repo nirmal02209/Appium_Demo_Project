43f94c07ae2b1c74cdc038818e7cfd35482a2460
nirmal02209 <nirmal02209@gmail.com>
2025-01-03 17:46:38 +0530

    push the code

--- src/main/java/com/example/appium/LoginTest.java
+++ src/main/java/com/example/appium/LoginTest.java
@@ -10,31 +10,27 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 public class LoginTest {
 
     @Test
-    public void testLogin() {
-        // Access the GitHub Secrets (set as environment variables)
+    public void printSecrets() {
+        Properties properties = new Properties();
+
+        // Load secrets from environment variables
         String username = System.getenv("USERNAME");
         String password = System.getenv("PASSWORD");
 
-        // Print the values to verify
-        System.out.println("USERNAME: " + username);
-        System.out.println("PASSWORD: " + password);
+        // Set properties
+        properties.setProperty("USERNAME", username);
+        properties.setProperty("PASSWORD", password);
 
-        // Print placeholders to confirm secrets are set
-        if (username != null && !username.isEmpty()) {
-            System.out.println("USERNAME is retrieved successfully.");
-        } else {
-            System.out.println("USERNAME is not set.");
-        }
-
-        if (password != null && !password.isEmpty()) {
-            System.out.println("PASSWORD is retrieved successfully.");
-        } else {
-            System.out.println("PASSWORD is not set.");
-        }
+        // Print the secrets
+        System.out.println("USERNAME: " + properties.getProperty("USERNAME"));
+        System.out.println("PASSWORD: " + properties.getProperty("PASSWORD"));
+    }
 
     }
-}
+
+
